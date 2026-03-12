eb69ab974dcaf6718072511383e569dc8d738c86
Benchetrit Binyamin <binou.ben26@gmail.com>
2026-03-12 17:59:18 +0200

    update promo video

--- src/App.jsx
+++ src/App.jsx
@@ -197,7 +197,7 @@ function App() {
             <Footer />
             
             {/* Widget d'accessibilité */}
-            <AccessibilityWidget />
+            {/* <AccessibilityWidget /> */}
           </div>
         </Router>
       </AuthProvider>
