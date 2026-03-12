4c48e4122e5a5b01d1b04018a0443f1d1ba38655
Benchetrit Binyamin <binou.ben26@gmail.com>
2026-03-12 16:05:02 +0200

    update promo video

--- src/components/home/PromoVideo.jsx
+++ src/components/home/PromoVideo.jsx
@@ -29,7 +29,7 @@ const promoTexts = [
   {
     main: "עדכנו את הפרטים שלכם בכל עת.",
      bgVideo: "/idkounpratim.mp4",
-    time: 7 
+    time: 6 
   },
   {
     main: "הלקוחות מדרגים את העבודה שלכם. הביקורות מחזקות את האמינות שלכם.",
